87d468864ebeed92063046137354b81a3b5334e2
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2017-04-07 09:57:26 +0200

    new function get_dbnomics

--- R/dbnomics.R
+++ R/dbnomics.R
@@ -1,6 +1,6 @@
 #' Get serie
 #'
-#' @param slug
+#' @param slug a string
 #'
 #' @return a tibble
 #' @export
@@ -9,16 +9,64 @@
 #' get_serie(slug = "bis-pp-ls-q-au")
 #'
 get_serie <- function(slug) {
-  base_url <- "https://api.db.nomics.world/api/v1/json/"
-  get_request <- httr::GET(url = paste0(base_url, "series/", slug)) %>%
-    httr::content() %>%
-    magrittr::extract2("data") %>%
-    magrittr::extract2("values")
+
+  get_request <- get_dbnomics(endpoint = "series", slug = slug) %>%
+  httr::content() %>%
+  magrittr::extract2("data") %>%
+  magrittr::extract2("values")
 
   tibble::tibble(
     period = purrr::map_chr(.x = get_request, .f = "period"),
     values = purrr::map_chr(.x = get_request, .f = "value")
   )
+
 }
 
+#' Get dbnomics
+#'
+#' @param endpoint a string either "providers", "datasets" or "series"
+#' @param slug a string name of the slug
+#' @param suffix a suffix. For instance "values"
+#'
+#' @return a get request
+#' @export
+#'
+#' @examples
+#'
+#' get_dbnomics(endpoint = "series", slug = "bis-pp-ls-q-au")
+#'
+#'
+#' # https://api.db.nomics.world/api/v1/json/datasets/insee-cna-2010-dep-apu/values
+#' get_dbnomics(
+#' endpoint = "datasets",
+#' slug = "insee-cna-2010-dep-apu",
+#' suffix = "values")
+#'
+#' \dontrun{
+#' library("magrittr")
+#' get_dbnomics(
+#' endpoint = "datasets",
+#' slug = "insee-cna-2010-dep-apu",
+#' suffix = "values") %>%
+#' httr::content() %>%
+#' magrittr::extract2("data")
+#' }
+#'
+
+get_dbnomics <- function(endpoint, slug, suffix) {
+
+  base_url <- "https://api.db.nomics.world/api/v1/json/"
+
+  url <-ifelse(
+    test = missing(suffix) == TRUE,
+    yes = paste0(base_url, endpoint, "/", slug),
+    no = paste0(base_url, endpoint, "/", slug, "/", suffix)
+    )
+
+  get_request <- httr::GET(url = url)
+
+  message(httr::status_code(get_request))
+
+  return(get_request)
+}
 
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 An R client to the [db.nomics.world](http://db.nomics.world/) database.
 
+## Documentation
 
+- [API Documentation](https://api.db.nomics.world/docs/json)
